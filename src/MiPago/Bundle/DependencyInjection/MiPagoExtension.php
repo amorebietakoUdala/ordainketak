d1c50d6aed3139df942e78d207ee1b37f78f3670
Amorebietako Udala <informatika@amorebieta.eus>
2019-01-31 14:16:25 +0100

    first commit

---
+++ src/MiPago/Bundle/DependencyInjection/MiPagoExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MiPago\Bundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ */
+class MiPagoExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+	$configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+	$loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+	
+	$definition = $container->getDefinition('MiPago\Bundle\Services\MiPagoService');
+	$definition->replaceArgument(1, $config['cpr']);
+	$definition->replaceArgument(2, $config['sender']);
+	$definition->replaceArgument(3, $config['format']);
+	$definition->replaceArgument(4, $config['suffix']);
+	$definition->replaceArgument(5, $config['language']);
+	$definition->replaceArgument(6, $config['return_url']);
+	$definition->replaceArgument(7, $config['test_environment']);
+//	$definition->replaceArgument(1, $config['payment_mode']);
+    }
+}
---
+++ src/MiPago/Bundle/MiPagoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MiPago\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MiPagoBundle extends Bundle
+{
+}
